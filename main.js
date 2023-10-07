de3be43804266ca0deb311f2ac9067e199b1de38
Leon Nicolhas <leonnhsalas@gmail.com>
2023-10-07 16:42:48 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,102 @@
+/* let diaDeLaSemana = prompt("¿Que día es hoy?");
+
+switch (diaDeLaSemana) {
+    case "lunes":
+        alert("Hoy es lunes,");
+        break;
+
+    case "martes":
+        alert("Hoy es martes,");
+        break;
+
+    case "miércoles":
+        alert("Hoy es miércoles,");
+        break;
+
+    case "jueves":
+        alert("Hoy es jueves,");
+        break;
+
+    case "viernes":
+        alert("Hoy es viernes,");
+        break;
+
+    case "sábado":
+        alert("Hoy es sábado,");
+        break;
+
+    case "domingo":
+        alert("Hoy es domingo,");
+        break;
+
+    default:
+        alert("Escribe el día de la semana en minúsculas o ")
+} */
+
+/* let numero = prompt("Escribe un número del 1 al 10");
+
+switch (numero) {
+    case "1":
+        alert("uno")
+        break;
+
+    case "2":
+        alert("dos")
+        break;
+    
+    case "3":
+        alert("tres")
+        break;
+        
+    case "4":
+        alert("cuatro")
+        break;
+        
+    case "5":
+        alert("cinco")
+        break;  
+        
+    case "6":
+        alert("seis")
+        break;
+        
+    case "7":
+        alert("siete")
+        break;
+        
+    case "8":
+        alert("ocho")
+        break;
+        
+    case "9":
+        alert("nueve")
+        break;
+        
+    case "10":
+        alert("diez")
+        break;
+    
+    default:
+        alert("Escribe un número del 1 al 10 en número")
+} */
+
+let sistema = prompt("¿ Qué sistema operativo usa ?")
+
+switch (sistema) {
+    case "Windows":
+        alert("Windows es el sistema operativo insignia de Microsoft, el estándar de facto para las computadoras domésticas y de negocios. El sistema operativo basado en una interfaz gráfica de usuario (GUI) fue introducido en 1985 y se han liberado muchas versiones desde entonces, como se describe a continuación.")
+        break;
+
+    case "Linux":
+        alert("Linux es un sistema operativo open source gratuito que se lanza en virtud de la Licencia Pública General de GNU (GPL). Cualquier persona puede ejecutar, estudiar, modificar o redistribuir el código fuente, e incluso comercializar las copias de su código modificado, siempre y cuando lo hagan con la misma licencia.")
+        break;
+
+    case "Mac":
+        alert("MAC es el acrónimo o abreviatura de Media Access Control y se refiere al conjunto de protocolos comunicacionales por medio del cual, varios equipos o dispositivos conectados a la red acuerdan usar conjuntamente un determinado medio de transmisión")
+        break;
+
+    default:
+        alert("Pon el sistema operativo que usas en mayúsculas la primera letra")
+        break;
+}
+
